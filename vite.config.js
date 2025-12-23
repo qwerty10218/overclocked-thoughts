0d6ea78088ea9bb93eb5e72e24ef07b05a506bd6
qwerty10218 <81663285+qwerty10218@users.noreply.github.com>
2025-12-23 20:59:05 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/<overclocked-thoughts>/', // 例如 base: '/garys-lab/', 注意前後要有斜線
+  base: '/overclocked-thoughts/', // 例如 base: '/garys-lab/', 注意前後要有斜線
 })
